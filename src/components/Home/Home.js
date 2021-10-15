91d2d12fc6c7900698087a1b68fcc67af2d5b521
onib0002 <onib0002@algonquinlive.com>
2021-10-14 21:22:26 -0400

    Homepage added

---
+++ src/components/Home/Home.js
@@ -0,0 +1,10 @@
+export default function Home(props){
+  return(
+    <div className = "home">
+      <p>Welcome to Home page</p>
+      <p>Kindly click on the User link to View random users and their details</p>
+      <p>The Address link gives you a quick view of User details</p>
+
+    </div>
+  )
+}
