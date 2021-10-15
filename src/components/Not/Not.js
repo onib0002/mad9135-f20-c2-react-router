9713497ca88c517daf9fe704977f031224715ce1
onib0002 <onib0002@algonquinlive.com>
2021-10-15 18:55:34 -0400

    Error page created

---
+++ src/components/Not/Not.js
@@ -0,0 +1,12 @@
+
+
+export default function Not(props){
+  return(
+    <div className = "not">
+      <h1 style={{ color:"red" }}>OOoooooooops!!!!!<br/>
+      <br/>
+      This is a 404 Page.</h1>
+
+    </div>
+  )
+}
