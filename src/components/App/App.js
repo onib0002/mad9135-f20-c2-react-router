1cab0068eed8a73972bf30af1fac432b132b972e
onib0002 <onib0002@algonquinlive.com>
2021-10-18 09:53:13 -0400

    Fetch function moved to Appjs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,9 +5,29 @@ import Users from '../Users/Users';
 import Address from '../Address/Address';
 import Not from '../Not/Not';
 import Navbar from '../Navbar/Navbar';
+import UserCard from '../UserCard/UserCard'
 import{Switch, Route, Redirect } from 'react-router-dom';
-
+import {useState, useEffect} from 'react'
 function App() {
+
+  //Moved fetch function here in order to have access to array of results
+
+  const [userList, setUserList] = useState([]);
+  async function fetchData(){
+    let url = 'https://randomuser.me/api/?seed=onib0002&results=32&format=json&nat=au,ca,nz,gb,us';
+    let resp = await fetch(url)
+    let data = await resp.json()
+
+    return setUserList(data.results);
+
+    //setUsers(data.results);//new array being put into list
+    //changing a state variable tells react to look and see
+    //if something needs to be re-rendered
+  }
+
+  useEffect(()=>{
+    fetchData()
+  }, [])
   return (
     <div className="App">
       <header>
@@ -16,11 +36,14 @@ function App() {
       <main>
         <Switch>
           <Route path = "/address">
-            <Address />
+            <Address userList={userList}/>
           </Route>
-          <Route path = "/users">
-            <Users /> 
-          </Route>   
+          <Route path = "/users" exact>
+            <Users userList={userList}/> 
+          </Route> 
+          <Route path = "/users/:id">
+            <UserCard userList={userList}/> 
+          </Route>    
           <Route path = "/404">
             <Not />
           </Route>
